5597f0ef8fe5de4ea78e413fb0f6147ee6468a0a
Marcos Paulo <marcospaulo.s.d@gmail.com>
2010-10-24 03:39:49 -0300

    Projeto reconstruido, commitando para inicio da aplicação.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinancialOnRails::Application.config.secret_token = 'aca6b95a85bf601e33033cb9f5525b10101d2f0e146d0102b7c8a7d3e03b2367161bcad42a24e58a77be343c6b46c24e040cbf20e32be5f511b337683c413c90'
