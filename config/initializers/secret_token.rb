63cc4237046d39d336706711bd9241ea6d7d282e
Marcos Paulo <marcospaulo.s.d@gmail.com>
2010-10-24 03:31:40 -0300

    Deletando todos os arquivos para poder recriar aplicação.
    Houve um erro com os scripts do rails.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FinancialOnRails::Application.config.secret_token = '4fcb2e8933c7176c40f2e407532aebd0b7d0c2dd311611ee51c4dd468fb239abe47b6d43b102913b3e68204e371f44cf9c4b8fd52823ae29ffe26f65fa9a42d8'
